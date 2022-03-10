6d2a21e3c1c8477f8273e8adfa1fb1adee9e791f
Dzennikk <denis_niktin@mail.ru>
2022-03-10 12:01:15 +0300

    После реализации конструктора с  параметрами

--- Practica1/Program.cs
+++ Practica1/Program.cs
@@ -4,35 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Practica1
+namespace Labal1
 {
-    class Firma
+    internal class Program
     {
-        public string name; //Название
-        public string adress; //Адрес
-        public int year; //Год основания
-        public string Name
+        class Firma
         {
-            get { return name; }
-            set { name = value; }
+            public Firma(string name, string street, int year) {
+                this.name = name; 
+                this.street = street; 
+                this.year = year; 
+            }
+            private string name;
+            private string street;
+            private int year;
+
+            public string Name {
+                get { return name; }
+                set { name = value; }
+            }
+            public string Street {
+                get { return street; }
+                set { street = value; }
+            }
+            public int Year {
+                get { return year; }
+                set { year = value; }
+            }
+            public void Vivod() => Console.WriteLine("Название: {0}\nАдрес: {1}\nГод основание: {2}", name, street, year);
         }
-        public string Adress
-        {
-            get { return adress; }
-            set { adress = value; }
-        }
-        public int Year
-        {
-            get { return year; }
-            set { year = value; }
-        }
-    }
- 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Firma a = new Firma(); 
+        static void Main(string[] args) {
+            Firma firma = new Firma("123123", "ул123123 32 корпус 4", 128);
+            firma.Vivod();
+            Console.ReadKey();
         }
     }
 }
