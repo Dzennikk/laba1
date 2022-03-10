3198040c2f11d6f57f5c5cb62f3e9bbb8c82b117
Dzennikk <denis_niktin@mail.ru>
2022-03-10 12:05:25 +0300

    изм

--- Practica1/Program.cs
+++ Practica1/Program.cs
@@ -10,28 +10,28 @@ namespace Labal1
     {
         class Firma
         {
-            public Firma(string name, string street, int year) {
+            public Firma(string name, string adress, int year) {
                 this.name = name; 
-                this.street = street; 
+                this.adress = adress; 
                 this.year = year; 
             }
             private string name;
-            private string street;
+            private string adress;
             private int year;
 
             public string Name {
                 get { return name; }
                 set { name = value; }
             }
-            public string Street {
-                get { return street; }
-                set { street = value; }
+            public string Adress {
+                get { return adress; }
+                set { adress = value; }
             }
             public int Year {
                 get { return year; }
                 set { year = value; }
             }
-            public void Vivod() => Console.WriteLine("Название: {0}\nАдрес: {1}\nГод основание: {2}", name, street, year);
+            public void Vivod() => Console.WriteLine("Название: {0}\nАдрес: {1}\nГод основание: {2}", name, adress, year);
         }
         static void Main(string[] args) {
             Firma firma = new Firma("123123", "ул123123 32 корпус 4", 128);
